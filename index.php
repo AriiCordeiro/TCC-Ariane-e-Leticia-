369e433a5c3d3e2f1424aff437edf30c91e03c48
AriiCordeiro <134703794+AriiCordeiro@users.noreply.github.com>
2023-06-16 14:03:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+    
+
+    if(isset($_POST['submit']));
+{
+    //print_r('<br>');
+   //print_r('Nome:' . $_POST['nome']);
+   // print_r('<br>');
+   // print_r('Contato: ' . $_POST['contato']);
+   // print_r('<br>');
+   // print_r('Email:' . $_POST['email']);
+
+   include_once('conexao.php');
+
+   $nome =  $_POST['nome'];
+   $contato = $_POST['contato'];
+   $email = $_POST['email'];
+
+   $result = mysqli_query($mysqli, "INSERT INTO tcc (nome, contato, email) VALUES ('$nome', '$contato', '$email')");
+}
+    
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,24 +28,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login do usuário</title>
-    <link rel="stylesheet" href="estilo1.css">
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <header>
-        <h1>Login</h1>
+        <hr></hr>
+        <label><h1>Cadastramento de usuário</h1></label> 
+        <br></br>
+        <center><h2>Informações do usuário:</h2></center> 
     </header>
     <section>
-        <form action="cad.php" method="get">
-        <label form="nome">Nome </label>
+        <form action="index.php" method="POST">
+        <label form="nome">Nome completo</label>
         <input type="text" name="nome" id="idnome">
         
-        <label form="contato">Contato</label>
+        <label form="contato">Contato pessoal</label>
         <input type="number" name="contato" id="idcontato">
 
-        <label form="email">Email</label>
+        <label form="email">E-mail institucional</label>
         <input type="text" name="email" id="idemail">
         
-        <input type="submit" value="Enviar">
+       <center> <input type="submit" name = "submit" id="submit"></center>
     </form>
     </section>
 </body>
