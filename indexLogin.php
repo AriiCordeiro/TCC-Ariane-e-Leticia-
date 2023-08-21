230362602f8781061bd585cf55d100ddb6c8883d
AriiCordeiro <134703794+AriiCordeiro@users.noreply.github.com>
2023-08-21 08:09:32 -0300

    Create indexLogin.php

---
+++ indexLogin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div class="imgEstilo" >
+    <img src="imagens/imgTcc.jpg"/></div>
+    <header>
+
+        <hr></hr>
+
+        <center><label><h1>Login</h1></label></center>
+        <br></br>
+         
+    </header>
+    <section>
+        <form action="index.php" method="POST">
+        
+
+        <label form="email">E-mail:</label>
+        <input type="text" name="email" id="idemail">
+
+        <label form="senha">Senha:</label>
+        <input type="text" name="senha" id="idsenha">
+
+        
+       <center> <input type="submit" name = "submit" id="submit"></center>
+    </form>
+    </section>
+</body>
+</html>
